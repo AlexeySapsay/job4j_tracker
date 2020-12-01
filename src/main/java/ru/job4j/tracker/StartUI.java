c30bb50dfe056db1533363dd1a717774bd98d3b5
AlekseySapsay <pheniksesalex@mail.ru>
2020-12-02 02:37:30 +0300

    9. toString [#402218]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,17 +4,20 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
 
-        Tracker tracker = new Tracker();
-
-        Item item1 = new Item();
-        item1.setName("TestName");
-
-        tracker.add(item);
-
-        System.out.println(tracker.findById(item.getId()));
-
-        tracker.replace(1,item);
-
-        System.out.println(tracker.findAll());
+//        Tracker tracker = new Tracker();
+//
+//        Item item1 = new Item();
+//        item1.setName("TestName");
+//
+//        tracker.add(item);
+//
+//        System.out.println(tracker.findById(item.getId()));
+//
+//        tracker.replace(1,item);
+//
+//        System.out.println(tracker.findAll());
+//
+        Item item2 = new Item();
+        System.out.println(item2);
     }
 }
