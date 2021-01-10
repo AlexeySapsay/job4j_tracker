95b2f2d465af130d629d9c85e73e86939ba9e394
AlekseySapsay <pheniksesalex@mail.ru>
2021-01-10 22:18:38 +0300

    Alphabet

---
+++ src/main/java/ru/job4j/list/Alphabet.java
@@ -0,0 +1,34 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Вам необходимо реализовать метод, который принимает строку, которое является одним словом, без знаков препинания цифр и пробелов. Метод должен вернуть новую строку, в которой все символы исходной строки будут расставлены в алфавитном порядке.
+ * <p>
+ * Для разделения строки на подстроки используйте метод split(), добавьте все элементы в коллекцию List<String>, отсортируйте в порядке возрастания - используйте метод sort, в который передадите компаратор. Новую строку сформируйте с помощью StringBuilder.
+ */
+
+
+public class Alphabet {
+    public static String reformat(String s) {
+        List<String> list = new ArrayList<>();
+        String[] splitteredStr = new String[s.length()];
+
+        splitteredStr = s.split("");
+
+        for (int i = 0; i < s.length(); i++) {
+            list.add(splitteredStr[i]);
+        }
+        Collections.sort(list);
+
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            str.append(list.get(i));
+        }
+        String string = str.toString();
+
+        return string;
+    }
+}
