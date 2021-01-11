fb6c2a61715d2c9b6b5ebdfe5c87b4e3ca820e33
AlekseySapsay <pheniksesalex@mail.ru>
2021-01-11 23:30:45 +0300

    2. Идентичные тексты [#402132]

---
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> rsl = new HashSet<Account>(accounts);
+        //HashSet<Account> rsl = new HashSet<>();
+        // задачу можно решить без цикла, просто передав в качестве параметра
+        // в HashSet accounts, но для тренировки навыка напишу цикл
+
+        /* for-each accounts to HashSet; */
+//        for(Account element : accounts){
+//            rsl.add(element);
+//        }
+
+        /* for-each accounts to HashSet; */
+
+
+//        for(Account element : accounts){
+//            System.out.println(rsl);
+//        }
+
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,102 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class NotifyAccountTest {
+
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+
+    @Test
+    public void sent2() {
+        List<Account> accounts = Arrays.asList(
+                new Account("345", "Alex Sapsay", "0007-0007"),
+                new Account("345", "Alex Sapsay", "0007-0007"),
+                new Account("3453", "Alex Sapsay", "0007-0007"),
+                new Account("3453", "Alex Sapsay", "0007-0007"),
+                new Account("0000", "Alex Sapsay", "6666-6666"),
+                new Account("0000", "Alex Sapsay", "9999-99999")
+        );
+
+        HashSet<Account> expect = new HashSet<>(Arrays.asList(
+                new Account("345", "Alex Sapsay", "0007-0007"),
+                new Account("3453", "Alex Sapsay", "0007-0007"),
+                new Account("0000", "Alex Sapsay", "6666-6666")
+
+        ));
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+
+    @Test
+    public void sent3() {
+        List<Account> accounts = Arrays.asList(
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007")
+        );
+
+        HashSet<Account> expect = new HashSet<>(Arrays.asList(
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Alex Sapsay", "0007-0007")
+        ));
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+
+    @Test
+    public void sent4() {
+        List<Account> accounts = Arrays.asList(
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Ivan Susanin", "7000-70000")
+        );
+
+        HashSet<Account> expect = new HashSet<>(Arrays.asList(
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Ivan Susanin", "7000-70000")
+        ));
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+
+    @Test
+    public void sent5() {
+        List<Account> accounts = Arrays.asList(
+                new Account("1111", "Alex Sapsay", "0007-0007"),
+                new Account("1111", "Ivan Susanin", "7000-70000")
+        );
+
+        HashSet<Account> expect = new HashSet<>(Arrays.asList(
+                new Account("1111", "Ivan Susanin", "7000-70000")));
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+}
+
+
