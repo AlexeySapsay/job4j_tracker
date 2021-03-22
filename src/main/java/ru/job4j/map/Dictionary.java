ae3942aa2d3f78bab5ca5af1f8f735f3ae001bb7
AlekseySapsay <pheniksesalex@mail.ru>
2021-03-23 02:54:08 +0300

    11. Реализовать словарь.

--- src/main/java/ru/job4j/map/Dictionary.java
+++ src/main/java/ru/job4j/map/Dictionary.java
@@ -23,64 +23,22 @@ public class Dictionary {
     public static Map<String, List<String>> collectData(String[] strings) {
         Map<String, List<String>> rsl = new HashMap<>();
         List<String> listEmpty = new ArrayList<String>();
-        //List<String> listBuffer = new ArrayList<String>();
-        //System.out.println(list);
-//        list.add("a");
-//        list.add("b");
-//        list.add("c");
 
-        // created a map and put the first character, converted to string = key,
-        // value = null
         for (String str : strings) {
-            //rsl.putIfAbsent(str.charAt(0), List < String > inputList = new ArrayList<String>());
-            //String strBuffer = toString()str.charAt(0);
+
             rsl.putIfAbsent(String.valueOf(str.charAt(0)), listEmpty);
         }
-
-//        for (String str : strings) {
-//            List<String> listBuffer = new ArrayList<String>(List.of());
-//            //rsl.computeIfPresent(String.valueOf(str.charAt(0)), (key, value) -> value = list);
-//            //rsl.computeIfPresent(String.valueOf(str.charAt(0)), (key, value) -> value = List.of("1", "2", "3"));
-//
-////            if((rsl.keySet().equals(String.valueOf(str.charAt(0))))){
-////                listEmpty.add(str);
-////            }
-//
-//            if(rsl.containsKey(String.valueOf(str.charAt(0)))){
-//                //List<String> listBuffer = new ArrayList<String>();
-//                listBuffer.add(str);
-//            }
-//
-//            rsl.computeIfPresent(String.valueOf(str.charAt(0)), (key, value) -> value = listBuffer);
-//
-//            //rsl.computeIfPresent(String.valueOf(str.charAt(0)), (key, value) -> value = list.add(str));
-//            System.out.println(rsl);
-//        }
 
         for (String strFromKeySet : rsl.keySet()) {
             List<String> listBuffer = new ArrayList<String>();
             for (String str : strings) {
                 if (strFromKeySet.equals(String.valueOf(str.charAt(0)))) {
-                    //System.out.println("somthing work");
                     listBuffer.add(str);
-                    //System.out.println(str);
-                    //System.out.println(listBuffer);
                 }
             }
             rsl.computeIfPresent(strFromKeySet, (key, value) -> value = listBuffer);
         }
 
-
-//        rsl.put("a", list);
-//        Map<String, List<String>> expected = Map.of(
-//                "s", List.of("six", "seven"),
-//                "t", List.of("two", "three"),
-//                "f", List.of("four", "five")
-//        );
-//        System.out.println("expected map:" + expected);
-
-        //System.out.println(rsl);
         return rsl;
     }
-
 }
