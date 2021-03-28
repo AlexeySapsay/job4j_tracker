074493538159255de609d83028d98a875f45acfe
AlekseySapsay <pheniksesalex@mail.ru>
2021-03-29 01:34:12 +0300

    3. Банковские переводы5 [#402128]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,6 +1,9 @@
 package ru.job4j.bank;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
 
 public class BankService {
     public static void main(String[] args) {
@@ -47,36 +50,36 @@ public class BankService {
 //    }
 
         // create account
-        List<Account> accounts = new ArrayList<>();
-        String requisite = "3fdsbb9";
-        accounts.add(new Account("3fdsbb9", 140));
+//        List<Account> accounts = new ArrayList<>();
+//        String requisite = "3fdsbb9";
+//        accounts.add(new Account("3fdsbb9", 140));
 //        int index = accounts.indexOf(new Account(requisite, -1));
 //        Account find = accounts.get(index);
 //        System.out.println(find.getRequisite() + " -> " + find.getBalance());
 //
-        int index = accounts.indexOf(new Account(requisite, -1));
-        Account find = accounts.get(index);
-        System.out.println(find.getRequisite() + " = " + find.getBalance());
+//        int index = accounts.indexOf(new Account(requisite, -1));
+//        Account find = accounts.get(index);
+//        System.out.println(find.getRequisite() + " = " + find.getBalance());
 
         //create account2
-        String requisite2 = "6660666";
-        accounts.add(new Account(requisite2, 100_000_000));
-        int index2 = accounts.indexOf(new Account(requisite2, -1));
-        Account find2 = accounts.get(index2);
-        System.out.println(find2.getRequisite() + " = " + find2.getBalance());
+//        String requisite2 = "6660666";
+//        accounts.add(new Account(requisite2, 100_000_000));
+//        int index2 = accounts.indexOf(new Account(requisite2, -1));
+//        Account find2 = accounts.get(index2);
+//        System.out.println(find2.getRequisite() + " = " + find2.getBalance());
 
 
         //test findByPassport
-        User user1 = new User("6006 155555", "Alex Saps");
-        BankService bank = new BankService();
-        bank.addUser(user1);
+//        User user1 = new User("6006 155555", "Alex Saps");
+//        BankService bank = new BankService();
+//        bank.addUser(user1);
 //        System.out.println("user1 изначальный : " + user1);
 //        System.out.println("user1 после помещения его в  bank.users" + bank.users);
 //        System.out.println("результат поиска user1 " +
 //                "через findByPassport" + bank.findByPassport("6006 155555") + "\n");
 
-        User user2 = new User("0000 0000", "Alex Navalniy");
-        bank.addUser(user2);
+//        User user2 = new User("0000 0000", "Alex Navalniy");
+//        bank.addUser(user2);
 //        System.out.println("user2 изначальный : " + user2);
 //        System.out.println("user2 после помещения его в  bank.users" + bank.users);
 //        System.out.println("user2 passport: " + user2.getPassport()
@@ -86,8 +89,8 @@ public class BankService {
         // его невозможно было перезаписать, если он существует
         // перезаписили юзера к которого был номер пасспорта 0000 0000 и имя "Alex Navalniy"
         // которого совпадает номер паспорта 0000 0000 и имя "Alex Ovalniy"
-        User user3 = new User("0000 0000", "Alex Ovalniy");
-        bank.addUser(user3);
+//        User user3 = new User("0000 0000", "Alex Ovalniy");
+//        bank.addUser(user3);
 //        System.out.println("user3 изначальный : " + user3);
 //        System.out.println("user3 после помещения его в  bank.users" + bank.users);
 //        System.out.println("результат поиска user3 " +
@@ -98,8 +101,8 @@ public class BankService {
 //        System.out.println("результат поиска user1 " +
 //                "через findByPassport : " + bank.findByPassport("6006 155555").getUsername() + "\n");
 
-        User user4 = new User("0000 0000", "Alex Ivanov");
-        bank.addUser(user4);
+//        User user4 = new User("0000 0000", "Alex Ivanov");
+//        bank.addUser(user4);
 //        System.out.println("результат поиска user4 " +
 //                "через findByPassport" + bank.findByPassport("0000 0000"));
 //        System.out.println("user4 passport: " + user4.getPassport()
@@ -128,6 +131,16 @@ public class BankService {
 //
 //        System.out.println("accounts" + accounts+
 //                accounts.get(0));
+
+
+        // test for transfer money
+
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        System.out.println((bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D)));
 
     }
 
@@ -211,7 +224,6 @@ public class BankService {
                 }
             }
         }
-
         return null;
     }
 
@@ -226,7 +238,63 @@ public class BankService {
                                  String destPassport,
                                  String destRequisite,
                                  double amount) {
-        boolean rsl = false;
+        //1.1 проверить существование источника по паспорту и реквизитам
+        //1.2 проверить существование принимающего по паспорту и реквизитам
+        if ((findByPassport(srcPassport) == null) ||
+                (findByRequisite(srcPassport, srcRequisite) == null) ||
+                (findByPassport(destPassport) == null) ||
+                (findByRequisite(destPassport, destRequisite) == null)) {
+            return false;
+        }
+
+        //2 несли не хватает денег на счете с которого переводят
+        // то возвращаем false
+        // проверка существование src account
+        Account srcAccount = new Account(null, -1);
+        User user1 = findByPassport(srcPassport);
+        if (user1 != null) {
+            List<Account> userAccount = users.get(user1);
+            for (Account account : userAccount) {
+                if (account.getRequisite().equals(srcRequisite)) {
+                    srcAccount = account;
+                }
+            }
+        }
+        // проверяем достаточно ли денег на счете, с которого выполняем перевод
+        // если не достаточно, то возвращаем false
+        if (srcAccount.getBalance() < amount) {
+            return false;
+        }
+
+        // проверка существование dest account
+        Account destAccount = new Account(null, -1);
+        User user2 = findByPassport(destPassport);
+        if (user2 != null) {
+            List<Account> userAccount = users.get(user2);
+            for (Account account : userAccount) {
+                if (account.getRequisite().equals(destRequisite)) {
+                    destAccount = account;
+                }
+            }
+        }
+
+
+        // 3 выполнить перевод с одного аккаунта на другой
+        // уменьшить значение в первом и прибавить значение во втором
+        double srcBalanceRaw = srcAccount.getBalance();
+        double srcBalanceWithoutAmount = srcBalanceRaw - amount;
+
+        double destBalanceRaw = destAccount.getBalance();
+        double destBalanceWithAmount = destBalanceRaw + amount;
+
+        srcAccount.setBalance(srcBalanceWithoutAmount);
+        destAccount.setBalance(destBalanceWithAmount);
+
+        System.out.println("srcAccount balance : " + srcAccount.getBalance());
+        System.out.println("destAccount balance : " + destAccount.getBalance());
+
+
+        boolean rsl = true;
         return rsl;
     }
 }
