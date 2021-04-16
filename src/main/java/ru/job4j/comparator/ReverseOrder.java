4c47ccf934af6fca9d68bbf224c234b9244d49e2
AlekseySapsay <pheniksesalex@mail.ru>
2021-04-16 21:56:59 +0300

    1.2. Получение компаратора. Метод reverseOrder()

---
+++ src/main/java/ru/job4j/comparator/ReverseOrder.java
@@ -0,0 +1,10 @@
+package ru.job4j.comparator;
+//https://job4j.ru/edu/task_code?topicId=30&taskCodeId=136&solutionId=new_task
+
+import java.util.Comparator;
+
+public class ReverseOrder {
+    public static Comparator<String> reverseOrder() {
+        return Comparator.reverseOrder();
+    }
+}
