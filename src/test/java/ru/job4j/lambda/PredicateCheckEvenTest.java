e432106bf1dbf116ffacee7be1afd1dc8b37f0a9
AlekseySapsay <pheniksesalex@mail.ru>
2021-06-03 22:05:22 +0300

    1.2. Predicate. Проверить, что число четное

---
+++ src/test/java/ru/job4j/lambda/PredicateCheckEvenTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.lambda;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PredicateCheckEvenTest {
+    @Test
+    public void test() {
+        assertTrue(PredicateCheckEven.check(2));
+        assertFalse(PredicateCheckEven.check(1));
+    }
+}
