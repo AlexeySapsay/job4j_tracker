1c1dd2e87263e4e0efbe3e6f6709f2d18a66c381
AlekseySapsay <pheniksesalex@mail.ru>
2021-06-10 00:20:09 +0300

    5. Преобразование матрицы чисел в список чисел. [#110228 #166244]

--- src/main/java/ru/job4j/stream/ConvertArrayIntegerToList1.java
+++ src/main/java/ru/job4j/stream/ConvertArrayIntegerToList1.java
@@ -1,13 +1,14 @@
 package ru.job4j.stream;
 
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ConvertArrayIntegerToList1 {
-    public List<Integer> convert(List<List<Integer>> matrix) {
-        return matrix.stream()
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+    public List<Integer> convert(Integer[][] matrix) {
+        return Stream.of(matrix).
+                flatMap(Arrays::stream).
+                collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ConvertArrayIntegerToList1Test.java
+++ src/test/java/ru/job4j/stream/ConvertArrayIntegerToList1Test.java
@@ -9,17 +9,12 @@ import static org.junit.Assert.*;
 public class ConvertArrayIntegerToList1Test {
     @Test
     public void whenConvertThanTrue() {
-        List<List<Integer>> matrix = List.of(
-                List.of(1, 2, 3),
-                List.of(3, 4, 5),
-                List.of(6, 7, 8)
-        );
+        Integer[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
         ConvertArrayIntegerToList1 sc = new ConvertArrayIntegerToList1();
-        List<Integer> rsl = sc.convert(matrix);
+        List<Integer> real = sc.convert(matrix);
         List<Integer> expected = List.of(
-                1, 2, 3, 3, 4, 5, 6, 7, 8);
-
-        assertEquals(expected, rsl);
+                1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertEquals(expected, real);
     }
 }
