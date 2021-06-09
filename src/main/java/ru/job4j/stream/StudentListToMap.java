49ca397049010974f3f567432c8ec2db60e2ec9d
AlekseySapsay <pheniksesalex@mail.ru>
2021-06-09 23:34:58 +0300

    4. Преобразование List в Map. [#110226 #166049]

--- src/main/java/ru/job4j/stream/StudentListToMap.java
+++ src/main/java/ru/job4j/stream/StudentListToMap.java
@@ -5,24 +5,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StudentListToMap {
-//    public Map<String, Student> collect(List<Student> studentList) {
-//        return studentList.stream()
-//                .distinct()
-//                .collect(Collectors.toMap(
-//                        Student::getSurname,
-//                        keyValue -> keyValue
-//                ));
-//    }
-
     public Map<String, Student> collect(List<Student> studentList) {
         return studentList.stream()
                 .collect(Collectors.toMap(
                         Student::getSurname,
                         keyValue -> keyValue,
-                        (keyValue1, keyValue2) -> {
-                            //System.out.println("duplicate key found!");
-                            return keyValue1;
-                        }
+                        (keyValue1, keyValue2) -> keyValue1
                 ));
     }
 }
