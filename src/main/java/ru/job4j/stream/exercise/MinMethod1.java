90bb7b639260d6c30fc9c6e85511907de9c5d5f6
AlekseySapsay <pheniksesalex@mail.ru>
2021-06-12 19:38:50 +0300

    fix task DepartmentsTest and MinMethod1.java

--- src/main/java/ru/job4j/stream/exercise/MinMethod1.java
+++ src/main/java/ru/job4j/stream/exercise/MinMethod1.java
@@ -1,12 +1,12 @@
 package ru.job4j.stream.exercise;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MinMethod1 {
     public static Integer min(List<Integer> list) {
         return list.stream()
-                .min((n1, n2) ->
-                        Integer.compare(n2, n1))
+                .min(Comparator.comparingInt(n -> n))
                 .get();
     }
 }
