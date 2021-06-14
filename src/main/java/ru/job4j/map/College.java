63e7c037758ea9d5f372d6081166e9d5aa77bd8d
AlekseySapsay <pheniksesalex@mail.ru>
2021-06-14 20:55:05 +0300

    3. Optional в банковских переводах. [#214711]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -11,106 +11,37 @@ public class College {
         this.students = students;
     }
 
-    //    public Student findByAccount(String account) {
-//            Student rsl = null;
-//        for (Student s : students.keySet()) {
-//            if (s.getAccount().equals(account)) {
-//                return s;
-//            }
-//        }
-//        return null;
-//    }
-
-//    public Student findByAccount(String account) {
-//        Student rsl = null;
-//        for (Student s: students.keySet()) {
-//            if (account.equals(s.getAccount())) {
-//                rsl = s;
-//                break;
-//            }
-//        }
-//        return rsl;
-//    }
-
-    // refactor that method for Optional
-//    public Student findByAccount(String account) {
-//        return students.keySet()
-//                .stream()
-//                .filter(s -> s.getAccount().equals(account))
-//                .findFirst()
-//                .orElse(null);
-//    }
-
-    // refactor that method for Optional
+    /**
+     * looking for account by String account.
+     * if account is present- return account
+     * if absent - return Optional.empty();
+     *
+     * @param account - user account
+     * @return Optional - return value
+     */
     public Optional<Student> findByAccount(String account) {
         Optional<Student> rsl = Optional.empty();
         for (Student s : students.keySet()) {
             if (account.equals(s.getAccount())) {
-                rsl = Optional.of(s);
-                break;
+                return Optional.of(s);
             }
         }
         return rsl;
     }
 
-//    public Subject findBySubjectName(String account, String name) {
-//        Student a = findByAccount(account);
-//        if (a != null) {
-//            Set<Subject> subjects = students.get(a);
-//            for (Subject s : subjects) {
-//                if (s.getName().equals(name)) {
-//                    return s;
-//                }
-//            }
-//        }
-//        return null;
-//    }
-
-//    public Subject findBySubjectName(String account, String name) {
-//        Student a = findByAccount(account);
-//        if (a != null) {
-//            return students.get(a)
-//                    .stream()
-//                    .filter(s -> s.getName().equals(name))
-//                    .findFirst()
-//                    .orElse(null);
-//        }
-//        return null;
-//    }
-
-//    public Subject findBySubjectName(String account, String name) {
-//        Subject rsl = null;
-//        Student s = findByAccount(account);
-//        if (s != null) {
-//            Set<Subject> subjects = students.get(s);
-//            for (Subject subj : subjects) {
-//                if (name.equals(subj.getName())) {
-//                    rsl = subj;
-//                    break;
-//                }
-//            }
-//        }
-//        return rsl;
-//    }
-
-//    public Optional<Subject> findBySubjectName(String account, String name) {
-//        Optional<Subject> rsl = Optional.empty();
-//        Optional<Student> student = findByAccount(account);
-//        if (student.isPresent()) {
-//            //if (student.isEmpty()) {
-//            Optional<Set<Subject>> subjects = Optional.ofNullable(students.get(student));
-//            //for (Optional<Set<Subject>> subj : subjects) {
-//            for (Subject subj : subjects) {
-//                if (name.equals(subj.getName())) {
-//                    //rsl = subj;
-//                    rsl = subj;
-//                    break;
-//                }
-//            }
-//        }
-//        return rsl;
-//    }
-
+    /**
+     * looking for subject by String account and name of subject.
+     * if account is present- return account
+     * if absent - return Optional.empty();
+     *
+     * if subject is present - return Optional Subject
+     * if subject is is absent - return Optional Subject empty
+     *
+     * @param account- user account
+     * @param name - name of subject by user
+     * @return if subject is present - return Optional Subject
+     *         if subject is is absent - return Optional Subject empty
+     */
     public Optional<Subject> findBySubjectName(String account, String name) {
         Optional<Subject> rsl = Optional.empty();
         Optional<Student> student = findByAccount(account);
@@ -118,13 +49,14 @@ public class College {
             Set<Subject> subjects = students.get(student.get());
             for (Subject subj : subjects) {
                 if (name.equals(subj.getName())) {
-                    rsl = Optional.of(subj);
-                    return rsl;
+                    return Optional.of(subj);
                 }
             }
         }
         return rsl;
     }
+
+// main method just for manual testing
 
 //    public static void main(String[] args) {
 //        Map<Student, Set<Subject>> students = Map.of(new Student("Student",
