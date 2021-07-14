29e11b63809db55af32e8e78b536f9d2edeb4cea
AlekseySapsay <pheniksesalex@mail.ru>
2021-07-14 09:59:56 +0300

    2.7. Сборка элементов. Сборка в строку

---
+++ src/main/java/ru/job4j/stream/exercise/JoiningMethod.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream.exercise;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Этот метод собирает строки в текст и добавляет Prefix and Suffix
+ */
+
+public class JoiningMethod {
+    public static String join(List<String> strings) {
+        return strings.stream()
+                .collect(Collectors.joining(System.lineSeparator(),
+                        "Prefix", "Suffix"));
+    }
+}
