35b74d02a80d5e7e5b3001a1b94eee9617fdae3d
AlekseySapsay <pheniksesalex@mail.ru>
2021-07-14 09:25:41 +0300

    2.5. Сборка элементов. Сборка в произвольную реализацию Collection

---
+++ src/main/java/ru/job4j/stream/exercise/Task3.java
@@ -0,0 +1,9 @@
+package ru.job4j.stream.exercise;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int a = 1234567891;
+        float b = a;
+        System.out.println(b);
+    }
+}
