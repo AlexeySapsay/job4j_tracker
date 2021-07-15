e4680bfc29b7dba34ff01d288a705e3062647f0c
AlekseySapsay <pheniksesalex@mail.ru>
2021-07-15 13:53:46 +0300

    9.1. Промежуточные операции. Метод sorted()

---
+++ src/main/java/ru/job4j/stream/exercise/SortedMethod.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream.exercise;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *In order to get a sorted stream, you can use the sorted () method.
+ * Note that the implementation of this method without input parameters
+ * uses the implemented compareTo () method of the Comparable interface.
+ * Therefore, if you are sorting objects of a specific class, you need to
+ * either pass a Comparator or implement Comparable
+ *
+ * For example,
+ *
+ * List.of (3, 2, 1) .stream (). Sorted () // get a stream from 1, 2, 3
+ *
+ * Your task is to get a list of sorted strings
+ *
+ */
+public class SortedMethod {
+    public static List<String> sorted(List<String> strings) {
+        return strings.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+}
