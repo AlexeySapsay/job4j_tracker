185633188c00f43533dbd2c8397cd8af3ff9f9f3
AlekseySapsay <pheniksesalex@mail.ru>
2021-07-15 11:40:31 +0300

    5.7. Промежуточные операции. Метод flatMapToInt(). Сумма чисел матрицы

---
+++ src/test/java/ru/job4j/stream/exercise/FlatMapForPrimitiveTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream.exercise;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FlatMapForPrimitiveTest {
+    @Test
+    public void test() {
+        assertEquals(15, FlatMapForPrimitive.
+                sum(new int[][]{{1, 2, 3}, {4, 5}}));
+    }
+}
