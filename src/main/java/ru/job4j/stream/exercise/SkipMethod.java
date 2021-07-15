424007ad9d574a729b953a1203c8fd15c85218b0
AlekseySapsay <pheniksesalex@mail.ru>
2021-07-15 13:22:53 +0300

    7.2. Промежуточные операции. Метод skip()

---
+++ src/main/java/ru/job4j/stream/exercise/SkipMethod.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream.exercise;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * To skip a certain number of stream elements, you can use the skip () method
+ * <p>
+ * For example,
+ * <p>
+ * List.of (1, 2, 3, 4, 5) .stream (). Skip (2) // will stream from items 3, 4, 5
+ * <p>
+ * Your task is to take and get the elements of the list, except for the first
+ * two
+ */
+public class SkipMethod {
+    public static List<String> lastThree(List<String> strings) {
+        return strings.stream()
+                .skip(2)
+                .collect(Collectors.toList());
+    }
+}
