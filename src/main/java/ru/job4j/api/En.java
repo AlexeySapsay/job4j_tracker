b5b1bea6e368a6b60e929833108954fe7440a426
AlekseySapsay <pheniksesalex@mail.ru>
2021-10-05 00:58:31 +0300

    streams exercise

---
+++ src/main/java/ru/job4j/api/En.java
@@ -0,0 +1,19 @@
+package ru.job4j.api;
+
+public class En {
+    //public enum Seasons
+    public static class Class1 {
+        public void sound() {
+            System.out.println("I love java");
+        }
+    }
+
+    public static class Class2 {
+
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        class1.sound();
+    }
+}
---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,33 @@
+package ru.job4j.stream;
+//https://job4j.ru/TrackStudio/staticframeset.html#401629
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**EasyStream работает только с типом Integer. В нем есть четыре метода
+
+ of - получает исходные данные.
+
+ map - преобразует число в другое число.
+
+ filter - фильтрует поток элементов.
+
+ collect - собирает все элементы из source по заданным условиям map и filter.
+**/
+public class EasyStream {
+    public static EasyStream of(List<Integer> source) {
+        throw new UnsupportedOperationException();
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        throw new UnsupportedOperationException();
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Integer> collect() {
+        throw new UnsupportedOperationException();
+    }
+}
